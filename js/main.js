098dff902fb71819d5d42c8bb862baa6f82e4c88
bowl13 <mauro@bowl.cl>
2013-07-09 19:21:23 -0400

    first

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+	//arrow key nav
+	$(".container-fluid").keyboardScroll({
+		scrollSpeed:1500
+	});
+
+	//carousel arrow nav
+	$(document).bind('keyup', function(e) {
+
+	  if(e.keyCode==39){
+		  jQuery('a.carousel-control.right').trigger('click');
+		}   
+	  else if(e.keyCode==37){
+		  jQuery('a.carousel-control.left').trigger('click');
+		}
+
+	});
+
+	$('#youtube1').mediaelementplayer();
+
+});
